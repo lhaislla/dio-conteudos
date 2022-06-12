60529e628d45dbdf9a40b8142dd927f8a52374eb
lhaislla <lhaislla@gmail.com>
2022-06-12 12:38:11 -0300

    Atividade Map

--- Modulo II/JS/Colecoes/maps.js
+++ Modulo II/JS/Colecoes/maps.js
@@ -10,12 +10,37 @@ const getAdmins = (map) => {
 	return admins;
 };
 
+const getusers= (map) => {
+	let users = [];
+
+	for ([key, value] of map) {
+		if (value === 'USER') {
+			users.push(key);
+		}
+	}
+
+	return users;
+};
+
+const getsudo= (map) => {
+	let sudo= [];
+
+	for ([key, value] of map) {
+		if (value === 'SUDO') {
+			sudo.push(key);
+		}
+	}
+
+	return sudo;
+};
 const userRoles = new Map();
 
-userRoles.set('Stephany', 'SUDO');
-userRoles.set('Luiz', 'ADMIN');
-userRoles.set('Elvira', 'ADMIN');
-userRoles.set('Carolina', 'USER');
-userRoles.set('Guilherme', 'USER');
+userRoles.set('Lhaíslla', 'SUDO');
+userRoles.set('Evelyn', 'ADMIN');
+userRoles.set('Thaíslla', 'ADMIN');
+userRoles.set('Matheus', 'USER');
+userRoles.set('Mayrê', 'USER');
 
-console.log(getAdmins(userRoles));
+console.log(`Administradores: `,getAdmins(userRoles));
+console.log(`Usuários: `,getusers(userRoles));
+console.log(`Sudo: `,getsudo(userRoles));
